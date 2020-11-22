f123147e948a52b65b935da907c69c0e14ddffe0
MarionCalpena <marioncalpe2a@gmail.com>
2020-11-22 21:42:27 +0100

    petit pause

--- src/Arme.java
+++ src/Arme.java
@@ -18,18 +18,4 @@ public class Arme {
     public int getPA() {
         return PA;
     }
-    public void ChoixArme(){
-        // System.out.println("Veuillez choisir une arme (pistolet, couteau, arc, fusil):");
-        // if (str == "pistolet"){
-        //     Joel.PA = Joel.PA + Pistolet.PA;
-        // }
-        // if(str == "couteau"){
-        //     Joel.PA = Joel.PA + Couteau.PA;
-        // }
-        // if(str == "arc"){
-        //     Joel.PA = Joel.PA + Arc.PA;
-        // }else{
-        //     Joel.PA = Joel.PA + Fusil.PA;
-        // }
-    }
 }
--- src/Combat.java
+++ src/Combat.java
@@ -19,20 +19,34 @@ public class Combat {
         return zombiePersonnage;
     }
     public Survivant choixSurvivant(Survivant survivant) throws IOException {
-        System.out.println("Choigisez votre Survivant pour le combat (Joel ou Ellie)");
+        System.out.println("Choigissez votre Survivant pour le combat (Joel ou Ellie)");
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
         String chaine = in.readLine();
         if (chaine == "Joel"){
-            survivant = Personnage.Joel;
+            survivant = Survivant.Joel;
         }if(chaine == "Ellie"){
-            survivant = survivant.Ellie;
+            survivant = Survivant.Ellie;
         }else{
-
+            //mettre une erreur ecrite et renvoyer la methode
         }
         return survivant;
     }
-    public void choixArme(Arme arme){
-
+    public Arme choixArme(Arme arme) throws IOException {
+        System.out.println("Choigissez votre Arme pour votre survivant (Pistolet, Couteau, Arc, Fusil)");
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        String chaine = in.readLine();
+        if (chaine == "Pistolet"){
+            arme = Arme.Pistolet;
+        }if(chaine == "Couteau"){
+            arme = Arme.Couteau;
+        if (chaine == "Arc"){
+            arme = Arme.Arc;
+        }if(chaine == "Fusil"){
+            arme = Arme.Fusil;
+        }else{
+            //mettre une erreur ecrite et renvoyer la methode
+        }
+        return arme;
     }
     public void combat() {
         if(personnageChoisie.PA > zombiePersonnage.PV){
